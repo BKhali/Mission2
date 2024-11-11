17457fa6d3f38df5f7e3a1534af02d827f2b1d0c
Khalil Bayech <khalilbayech@gmail.com>
2024-11-11 13:38:08 +0100

    jenpeutplus

--- Mission2/DAO/traverseeDAO.cs
+++ Mission2/DAO/traverseeDAO.cs
@@ -59,7 +59,7 @@ namespace Connecte.DAO
             }
         }
 
-        public static void inserttraverse(int id_liaison, int id_traverse, int id_bateau, string Datetraverse,int heure)
+        public static void inserttraverse(int id_liaison, int id_traverse, int id_bateau, string Datetraverse, int heure)
         {
 
             try
@@ -72,7 +72,7 @@ namespace Connecte.DAO
                 maConnexionSql.openConnection();
 
 
-                Ocom = maConnexionSql.reqExec("INSERT INTO traverse VALUES(" + id_liaison + ","+ id_traverse +","+ id_bateau + "," + Datetraverse + "," + heure);
+                Ocom = maConnexionSql.reqExec("INSERT INTO traverse VALUES(" + id_liaison + "," + id_traverse + "," + id_bateau + "," + Datetraverse + "," + heure);
 
                 int i = Ocom.ExecuteNonQuery();
 
@@ -178,235 +178,12 @@ namespace Connecte.DAO
 
             }
 
-
-<<<<<<< HEAD
-
-
-
-            /**
-            public static List<traversee> getTraversee()
-=======
         }
-        /**
-        public static List<traversee> getTraversee()
-        {
-
-            List<traversee> lc = new List<traversee>();
-
-            try
->>>>>>> 21f9de5802b56f164384a3383f1e70712296b376
-            {
-
-                List<traversee> lc = new List<traversee>();
-
-                try
-                {
-
-                    maConnexionSql = ConnexionSql.getInstance(provider, dataBase, uid, mdp);
-
-
-                    maConnexionSql.openConnection();
-
-
-                    Ocom = maConnexionSql.reqExec("SELECT l.ID_LIAISON AS liaison, t.ID_TRAVERSE, b.ID_BATEAU AS bateau, t.DATETRAVERSE, t.HEURE " +
-                        "FROM traverse t" +
-                        "JOIN liaison l ON l.ID_LIAISON = t.ID_LIAISON" +
-                        "JOIN bateau b ON b.ID_BATEAU = t.ID_BATEAU; ");
-
-
-                    MySqlDataReader reader = Ocom.ExecuteReader();
-
-                    traversee e;
-
-
-
-
-                    while (reader.Read())
-                    {
-
-                        liaison id_liaison = (liaison)reader.GetValue(0);
-                        int id = (int)reader.GetValue(1);
-                        bateau id_bateau = (bateau)reader.GetValue(2);
-                        string date = (string)reader.GetValue(3);
-                        int heure = (int)reader.GetValue(4);
-
-                        //Instanciation
-                        e = new traversee(id, id_liaison, id_bateau, date, heure);
-
-                        // Ajout à la liste 
-                        lc.Add(e);
-
-
-                    }
-
-
-
-                    reader.Close();
-
-                    maConnexionSql.closeConnection();
-
-                    // Envoi de la liste
-                    return (lc);
-
-
-                }
-
-                catch (Exception emp)
-                {
-
-                    throw (emp);
-
-                }
-
-
-            }
-            /**
-            public static void supprEmploye(Employe e)
-            {
-
-                try
-                {
-
-
-                    maConnexionSql = ConnexionSql.getInstance(provider, dataBase, uid, mdp);
-
-
-                    maConnexionSql.openConnection();
-
-
-                    Ocom = maConnexionSql.reqExec("DELETE FROM employe WHERE id=" + e.Id);
-
-
-                    int i = Ocom.ExecuteNonQuery();
-
-
-
-                    maConnexionSql.closeConnection();
-
-
-
-                }
-
-                catch (Exception emp)
-                {
-
-                    throw (emp);
-                }
-
-
-            }
-            public static void insertEmploye(int id, string login)
-            {
-
-                try
-                {
-
-
-                    maConnexionSql = ConnexionSql.getInstance(provider, dataBase, uid, mdp);
-
-
-                    maConnexionSql.openConnection();
-
-
-                    Ocom = maConnexionSql.reqExec("INSERT INTO employe VALUES (" + id + ",'','','" + login + "',0)");
-
-
-                    int i = Ocom.ExecuteNonQuery();
-
-
-
-                    maConnexionSql.closeConnection();
-
-
-
-                }
-
-                catch (Exception emp)
-                {
-
-                    throw (emp);
-                }
-
-
-            }**/
-        }
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 72123d54fe636f027fb2e36ac64bdeda2e5f1ced
-        /**
-        public static void supprEmploye(Employe e)
-        {
-
-            try
-            {
-
-
-                maConnexionSql = ConnexionSql.getInstance(provider, dataBase, uid, mdp);
-
-
-                maConnexionSql.openConnection();
-
-
-                Ocom = maConnexionSql.reqExec("DELETE FROM employe WHERE id=" + e.Id);
-
-
-                int i = Ocom.ExecuteNonQuery();
-
-
-
-                maConnexionSql.closeConnection();
-
-
-
-            }
-
-            catch (Exception emp)
-            {
-
-                throw (emp);
-            }
-
-
-        }
-        public static void insertEmploye(int id, string login)
-        {
-
-            try
-            {
-
-
-                maConnexionSql = ConnexionSql.getInstance(provider, dataBase, uid, mdp);
-
-
-                maConnexionSql.openConnection();
-
-
-                Ocom = maConnexionSql.reqExec("INSERT INTO employe VALUES (" + id + ",'','','" + login + "',0)");
-
-
-                int i = Ocom.ExecuteNonQuery();
-
-
-
-                maConnexionSql.closeConnection();
-
-
-
-            }
-
-            catch (Exception emp)
-            {
-
-                throw (emp);
-            }
-
-
-        }**/
     }
->>>>>>> 21f9de5802b56f164384a3383f1e70712296b376
+
 
 }
+
 
 
 
